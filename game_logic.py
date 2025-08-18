527a7203943e8a2c351d7a31dfd65ebf96bac2b0
Moritz Brosch <moritzbrosch@Mac.fritz.box>
2025-08-18 12:30:59 +0200

    All game logic functions have been moved to a new file named: game_logic.py

---
+++ game_logic.py
@@ -0,0 +1,63 @@
+import random
+from ascii_art import STAGES
+# List of secret words
+WORDS = ["python", "git", "github", "snowman", "meltdown"]
+
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    """Display the mistakes"""
+    print(STAGES[mistakes])
+
+    display_word = "".join([letter if letter in guessed_letters else "_"
+                            for letter in secret_word])
+    print("Word: " + display_word)
+    print("Mistakes: " + str(mistakes))
+    print("Guessed letters:", " ".join(sorted(guessed_letters)))
+    print()
+
+
+def get_random_word():
+    """Selects a random word from the list."""
+    return WORDS[random.randint(0, len(WORDS) - 1)]
+
+
+def play_game():
+    secret_word = get_random_word()
+    guessed_letters = set()
+    mistakes = 0
+    max_mistakes = len(STAGES) - 1
+
+    print("Welcome to Snowman Meltdown!")
+
+    while mistakes < max_mistakes:
+        display_game_state(mistakes, secret_word, guessed_letters)
+
+        # TODO: Build your game loop here.
+        # For now, simply prompt the user once:
+        guess = input("Guess a letter: ").lower()
+
+        print("Secret word selected: " + secret_word)  # for testing, later remove this line
+
+        if not guess.isalpha() or len(guess) != 1:
+            print("Please only letters!")
+            continue
+
+        if guess in guessed_letters:
+            print("You've already guessed the letter!")
+            continue
+
+        guessed_letters.add(guess)
+
+        if guess not in secret_word:
+            mistakes += 1
+
+
+        if all(letter in guessed_letters for letter in secret_word):
+            display_game_state(mistakes, secret_word, guessed_letters)
+            print("You win! The word was:", secret_word)
+            return
+
+        print("You guessed:", guess)
+
+    display_game_state(mistakes, secret_word, guessed_letters)
+    print("You lost! The word was:", secret_word)
